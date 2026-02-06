e93db5920d89e29748d23e35372585c16543f70b
ayunahkim <79731952+ayunahkim@users.noreply.github.com>
2026-02-06 11:57:14 +0900

    finishing galaxy hw

--- class_002_galaxies/src/main.js
+++ class_002_galaxies/src/main.js
@@ -18,16 +18,19 @@ function init(){
   document.body.appendChild(renderer.domElement);
   camera.position.z = 5;
 
-  meshes.sphere = addSphereMesh(.4,'skyblue');
-  meshes.sphere.position.x = -2;
+  meshes.sphere = addSphereMesh(.3,'skyblue');
   meshes.sphere2 = addSphereMesh(1,'yellow');
-  meshes.sphere3 = addSphereMesh(.5,'salmon');
-  meshes.sphere3.position.x = 1;
+  meshes.sphere3 = addSphereMesh(.2,'salmon');
+  meshes.sphere4 = addSphereMesh(.3,'aliceblue');
+  meshes.sphere5 = addSphereMesh(.2,'aquamarine');
+  meshes.sphere6 = addSphereMesh(.2,'mediumpurple');
 
-  scene.add(meshes.default);
   scene.add(meshes.sphere);
   scene.add(meshes.sphere2);
   scene.add(meshes.sphere3);
+  scene.add(meshes.sphere4);
+  scene.add(meshes.sphere5);
+  scene.add(meshes.sphere6);
   animate();
 }
 
@@ -37,8 +40,18 @@ function animate(){
   renderer.render(scene,camera);
   tick+=0.01;
 
-  meshes.sphere.position.x = Math.sin(tick*2)*3;
-  meshes.sphere.position.y = Math.cos(tick*2)*3;
-  meshes.sphere3.position.x = Math.sin(tick*3)*-2;
-  meshes.sphere3.position.y = Math.cos(tick*3)*-2;
+  meshes.sphere.position.x = Math.sin(tick*.5)*3;
+  meshes.sphere.position.y = Math.cos(tick*.5)*3;
+
+  meshes.sphere3.position.x = Math.sin(tick)*2;
+  meshes.sphere3.position.y = Math.cos(tick)*2;
+
+  meshes.sphere4.position.x = Math.sin(tick*3)*1.5;
+  meshes.sphere4.position.y = Math.cos(tick*3)*1.5;
+
+  meshes.sphere5.position.x = Math.sin(tick*2)*3;
+  meshes.sphere5.position.y = Math.cos(tick*2)*3;
+
+  meshes.sphere6.position.x = Math.sin(tick)*4;
+  meshes.sphere6.position.y = Math.cos(tick)*4;
 }
